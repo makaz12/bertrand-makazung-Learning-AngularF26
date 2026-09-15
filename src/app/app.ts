6fbe1cb703bafb34ef81a0c39297183208b08977
makaz <makazungbertrand@gmail.com>
2026-09-14 20:08:17 -0400

    Created starter Angular files and completed the Assignment 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('bertrand-makazung-Learning-AngularF26');
+  amount: number = 5;
+  plant: string = "Oak tree"
+
 }
